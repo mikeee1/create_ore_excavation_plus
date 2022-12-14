a2733a98af5d0af2f4ae5718d23cb53f5792a00e
mikeee1 <mikevansmaalen@gmail.com>
2022-12-14 20:56:55 +0100

    did stuff

--- src/main/java/com/mike/create_ore_excavation_plus/item/ModItems.java
+++ src/main/java/com/mike/create_ore_excavation_plus/item/ModItems.java
@@ -14,11 +14,23 @@ public class ModItems {
 
     public static final RegistryObject<Item> cut_raw_diamond = ITEMS.register("cut_raw_diamond",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> heated_raw_diamond = ITEMS.register("heated_raw_diamond",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> raw_diamond_dust = ITEMS.register("raw_diamond_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> washed_raw_diamond = ITEMS.register("washed_raw_diamond",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
     public static final RegistryObject<Item> pressed_raw_diamond = ITEMS.register("pressed_raw_diamond",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
     public static final RegistryObject<Item> cut_raw_emerald = ITEMS.register("cut_raw_emerald",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
     public static final RegistryObject<Item> pressed_raw_emerald = ITEMS.register("pressed_raw_emerald",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> heated_raw_emerald = ITEMS.register("heated_raw_emerald",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> raw_emerald_dust = ITEMS.register("raw_emerald_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
+    public static final RegistryObject<Item> washed_raw_emerald = ITEMS.register("washed_raw_emerald",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.create_ore_excavation_plus_tab)));
 
 
